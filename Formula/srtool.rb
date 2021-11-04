f112013a2ddcf84aa3663cf35c657975f368e293
TeraBot <chevdor@users.noreply.github.com>
2021-11-04 13:03:33 +0000

    build: new homebrew formula for v0.7.1

--- Formula/srtool.rb
+++ Formula/srtool.rb
@@ -1,9 +1,9 @@
 class Srtool < Formula
   desc "A command line utility to easily use the srtool docker image"
   homepage "https://github.com/chevdor/srtool-cli"
-  url "https://github.com/chevdor/srtool-cli/releases/download/v0.7.0/srtool_macos_v0.7.0.tar.gz"
-  sha256 "b37e488600129712e2d6024e79c0a13fab9de80a9d8b9e6ac794c967b96fbf8a"
-  version "0.7.0"
+  url "https://github.com/chevdor/srtool-cli/releases/download/v0.7.1/srtool_macos_v0.7.1.tar.gz"
+  sha256 "92c26d6337efcb4718ae7cd580e0ee63cbefff812dcb7ea4571addaa23ec9020"
+  version "0.7.1"
 
   def install
     bin.install "srtool"
