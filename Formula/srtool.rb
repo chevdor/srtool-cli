7d6590afcd048e15ddb4915062ad6cef5ad83d34
Chevdor <chevdor@gmail.com>
2022-10-10 09:46:39 +0200

    Fix some CI issues

--- Formula/srtool.rb
+++ Formula/srtool.rb
@@ -1,9 +1,9 @@
 class Srtool < Formula
   desc "A command line utility to easily use the srtool docker image"
   homepage "https://github.com/chevdor/srtool-cli"
-  url "https://github.com/chevdor/srtool-cli/releases/download/v0.8.0/srtool_macos_v0.8.0.tar.gz"
-  sha256 "1a52a779f87ce3bd8ba25a4a91a491bcc29dfa6d5e79a284c6bd6193aa1e55ef"
-  version "0.8.0"
+  url "https://github.com/chevdor/srtool-cli/releases/download/v0.9.0/srtool_macos_v0.9.0.tar.gz"
+  sha256 "8ac0980b0662283316b4fd06adbfba9c6e8695c0c5c6f6b89163092e650b271f"
+  version "0.9.0"
 
   def install
     bin.install "srtool"
